4693a866939478e5996a68d249531340a202b518
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-12-08 10:38:22 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,18 @@
+import com.sun.source.tree.BreakTree;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyTest {
     @Test
-    void name() {
-
+    public void correctValue() {
+        assertEquals("codewa.rs", Kata.name);
     }
-}
+
+
+    private static class Kata {
+        public static String name = "codewa.rs";
+        }
+    }
+
+
